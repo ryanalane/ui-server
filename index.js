1e74f11775df5da571da429c9ba33d22a9b2bb27
Ryan Lane <ryan.lane@lab.coop>
2016-05-16 19:44:06 +0200

    Swapped out manual fileserving for the express.static module

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ var app = express();
 // Set environment
 app.set('port', (process.env.PORT || 5000));
 
-// Legacy routes for UI prototypes
-app.get('/:file_name', function(request, response) {
-  response.sendFile('./' + request.params.file_name, { root: __dirname});
-});
+// Routes for static files
+app.use(express.static(__dirname));
 
 // Sample API GET request handler
 app.get('/front_end_api/accounts/current', function(request, response) {
