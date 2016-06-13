053f1326d6171fc6d2b3590881bc375cb02876b0
Ryan Lane <ryan.lane@lab.coop>
2016-06-13 15:25:42 +0200

    Removed unused variable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 
 var path = require('path');
-var appRootDirectory = path.dirname(require.main.filename);
 
 var app = express();
 
@@ -13,12 +12,17 @@ app.use(express.static(__dirname));
 
 // Sample API GET request handler
 app.get('/front_end_api/accounts/current', function(request, response) {
-  response.status(201).send({"user":{"name":"Name of Current User"}});
+  response.status(201).send({"name":"Name of Current User"});
+  console.log('the Backbone bro came through :)');
 });
 
 // Sample API POST request handler
 app.post('/front_end_api/session', function(request, response) {
   response.status(201).send();
+});
+app.delete('/front_end_api/session', function(request, response) {
+  response.status(201).send();
+  console.log('and someone signed out!');
 });
 
 app.listen(app.get('port'), function(){
